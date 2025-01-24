6d5dbae33d20d1a239c772782e76ec55627acebb
Gonzalo Fonseca <gonzafonzeca16@gmail.com>
2025-01-24 01:16:31 -0300

    Version final

---
+++ app.js
@@ -0,0 +1,44 @@
+let friends = [];
+
+function addFriend() {
+    let friend = document.querySelector('#amigo').value;
+    if (friend === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    
+    friends.push(friend);
+    document.querySelector('#amigo').value = '';
+    //console.log(friends);
+    showFriends();
+    
+}
+list = document.getElementById('listaAmigos');
+result = document.getElementById('resultado');
+function showFriends() {
+
+    
+    list.innerHTML = "";
+    friends.forEach(friend => {
+        //console.log("Amigo: " + friend)
+        let li = document.createElement('li');
+        li.textContent = friend;
+        list.appendChild(li);
+    });
+}
+
+
+function sortFriend() {
+
+    if (friends.length == 0) {
+        alert('La lista esta vacia');
+        return;
+    }
+    let generateNumber = Math.floor(Math.random()*friends.length)+1;
+    
+    list.innerHTML = "";
+    result.textContent = "El amigo sorteado es: " + friends[generateNumber];
+
+}
+
